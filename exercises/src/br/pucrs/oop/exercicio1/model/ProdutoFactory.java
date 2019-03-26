700f6feea27470a7dd5414e3699f2e2f29533d47
Pércio Reinert <percioreinert@gmail.com>
2019-03-26 13:38:25 -0300

    Finished implementations.

--- exercises/src/br/pucrs/oop/exercicio1/model/ProdutoFactory.java
+++
@@ -1,6 +0,0 @@
-package br.pucrs.oop.exercicio1.model;
-
-public interface ProdutoFactory {
-
-    Produto getProduto(String type);
-}
--- exercises/src/br/pucrs/oop/exercicio1/util/NomesProduto.java
+++
@@ -1,9 +0,0 @@
-package br.pucrs.oop.exercicio1.util;
-
-public class NomesProduto {
-
-    public static final String P1 = "";
-    public static final String P2 = "";
-    public static final String P3 = "";
-    public static final String P4 = "";
-}
